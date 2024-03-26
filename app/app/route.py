833e5cb7945d23dd4b2c94e6ddf5e745e30df49d
BE-02-KMJ <mjk5912@naver.com>
2024-03-26 18:05:58 +0900

    chatting model done

--- app/app/route.py
+++ app/app/route.py
@@ -1,16 +1,17 @@
-# channels Library를 활용해서 Socket 연결하는 비동기 server(route) 구현 
+# channels 라이브러리를 활용해서 Socket 연결하는 비동기 라우트 구현
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat.routes import websocket_urlpatterns
-from djnago.core.asgi import get_asgi_application
+from django.core.asgi import get_asgi_application
+
+from chat.routing import websocket_urlpatterns
+
 import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application,
-    # (http->django views is added by default)
-    'websocket': AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)    # ws://127.0.0.1:8000/ws/room
+    'http': get_asgi_application(),
+    'websocket':AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns) # ws://127.0.0.1:8000/ws/{room_id}
     )
 })
